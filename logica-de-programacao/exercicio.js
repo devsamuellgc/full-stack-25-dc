bc633b14b31766d507ca3cfa02c731c0e11003c4
devsamuellgc <samuelgalrao@hotmail.com>
2023-10-17 20:06:35 -0300

    lógica

---
+++ logica-de-programacao/exercicio.js
@@ -0,0 +1,44 @@
+// // Par ou ímpar
+// const num = 0;
+
+// if (num === 0) {
+//   console.log("Número zero");
+// } else if (num % 2 === 0) {
+//   console.log("Número par");
+// } else {
+//   console.log("Número ímpar");
+// }
+
+// // Idade para dirigir
+
+// const idade = 17;
+
+// if (idade >= 18) {
+//   console.log("Permitido dirigir");
+// } else {
+//   console.log("Não permitido dirigir");
+// }
+
+// // Calculadora de bônus
+
+// const tempoServico = 10;
+// const salario = 3000;
+
+// if (tempoServico < 2) {
+//   console.log(`Salário: ${salario}`);
+// } else if (tempoServico < 5) {
+//   console.log(`Salário: ${salario * 1.05}`);
+// } else if (tempoServico < 10) {
+//   console.log(`Salário: ${salario * 1.1} ${tempoServico}`);
+// } else {
+//   console.log(`Salário: ${salario * 1.15} ${tempoServico}`);
+// }
+
+// const temp = 89.6;
+// const type = "fahrenheit";
+
+// if (type === "celcius") {
+//   console.log(temp * 1.8 + 32);
+// } else if (type === "fahrenheit") {
+//   console.log((temp - 32) * 0.5555555 );
+// }
---
+++ logica-de-programacao/index.js
@@ -0,0 +1,38 @@
+// if (1 > 5) {
+//   console.log("Número maior");
+// } else if (1 === 2) {
+//   console.log("Número igual");
+// } else {
+//   console.log("Número menor");
+// }
+
+/**
+ * Número maior, menor ou zero
+ */
+
+// let valor = 1;
+// const valor2 = 0;
+// valor = 2;
+// if (valor > 0) {
+//   console.log("Positivo");
+// } else if (valor < 0) {
+//   console.log("Negativo");
+// } else {
+//   console.log("Zero");
+// }
+
+/**
+ * = -> Equivalência / atribuição
+ * == -> Compara valores
+ * === -> Compara valores e tipo
+ */
+
+// console.log("1" === 1);
+
+// Operador ternário
+// const resultado = false;
+// const resultado2 = -3;
+
+// console.log(!resultado);
+
+// !resultado ? console.log("Verdadeiro") : console.log("Falso");
